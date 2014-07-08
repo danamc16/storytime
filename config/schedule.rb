25fa6f1cf6823db05f72199003cbff443efff03d
Dana Chaykovsky <dana.chaykovsky@yale.edu>
2014-07-08 10:25:58 -0400

    Fleshed out change_story and normal functions; included age function

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,23 +19,39 @@
 
 # Learn more: http://github.com/javan/whenever
 
-def default
-	adjusted = Datetime.now().advance(:hours => -24)
-  	if @user.created_at <= adjusted
-	  	every 1.day, :at => @user.time do
-  			#send default story
-  			#Story.first = default story
-  			#Story.first.activities = default activities
+class Functions
+	def default
+		adjusted = Datetime.now().advance(:hours => -24)
+	  	if @user.created_at <= adjusted
+		  	every 1.day, :at => @user.time do
+	  			#send default story
+	  			#Story.first = default story
+	  			#Story.first.activities = default activities
+			end
+	  	end
+	end
+
+	def change_story
+		if Story.find_by((@user.story.id) + 1).title != @user.story.title
+			puts Story.second + Story.second.activities
 		end
-  	end
-end
 
-def change_story
-end
+	end
 
-def normal
+	def normal
+		if Story.find_by((@user.story.id) + 1).title == @user.story.title
+			puts 'Here\'s your next chapter.'
+		end
+	end
 end
 
 every 1.day do
-	command "."
+	runner "Functions.default"
+	runner "Functions.normal"
+	runner "Functions.change_story"
 end
+
+	def age(dob)
+		now = Time.now.utc.to_date
+		now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+	end
